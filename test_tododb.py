a8dc8d44eadc4c6e501a3b9ba5d778320e9d6a1c
Laurent Peuch <psycojoker@gmail.com>
2010-05-31 20:18:50 +0200

    Oups, forget to said the file was rename to git, change the API, now there is only one big TodoDB class and everything is done on it

--- test_tododb.py
+++ test_tododb.py
@@ -3,20 +3,28 @@
 
 
 import unittest
-import tododb
+from tododb import TodoDB
 
 class MaTest(unittest.TestCase):
+
     def reinitialise(self):
+        tododb = TodoDB()
         tododb.connect()
         tododb.drop_db()
         tododb.create_db()
+        return tododb
 
     def test_create_a_db(self):
-        self.reinitialise()
+        tododb = self.reinitialise()
 
     def test_add_a_todo(self):
-        self.reinitialise()
+        tododb = self.reinitialise()
 
+        was = tododb.todo_len()
+
+        tododb.add_todo("This is a new todo")
+
+        self.assertEqual(was + 1, tododb.todo_len())
 
 if __name__ == "__main__":
    unittest.main()
---
+++ tododb.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from sqlobject import *
+
+#from datetime import datetime
+
+def _select_len(select):
+    len = 0
+    for i in select:
+        len += 1
+
+    return len
+
+class TodoDB(object):
+    def __init__(self):
+        pass
+
+    #class Context(SQLObject):
+        #description = StringCol()
+        #position = IntCol(unique=True)
+        #hide = BoolCol(default=False)
+        #created_at = DateTimeCol(default=datetime.now())
+        #completed_at = DateTimeCol(default=None)
+
+    #class Project(SQLObject):
+        #description = StringCol()
+        #state = EnumCol(enumValues=('active', 'completed', 'hidden'), default="active")
+        #created_at = DateTimeCol(default=datetime.now())
+        #completed_at = DateTimeCol(default=None)
+        #tickler = DateCol(default=None)
+        #due = DateCol(default=None)
+        #default_context_id = IntCol(default=None)
+
+    #class Item(SQLObject):
+        #description = StringCol()
+        #context = ForeignKey('Context')
+        #project = IntCol(default=None)
+        #created_at = DateTimeCol(default=datetime.now())
+        #hidden = BoolCol(default=False)
+        ##tickler = DateCol(default=None)
+        ##next_todo = IntCol(default=None)
+        ##previous_todo = IntCol(default=None)
+
+    class _Todo(SQLObject):
+        """
+        A Todo
+
+        Arguments:
+            * description: a text field that discribe the todo
+        """
+        description = StringCol()
+        #notes = StringCol(default=None)
+        #context = ForeignKey('Context')
+        #project = IntCol(default=None)
+        #created_at = DateTimeCol(default=datetime.now())
+        #completed_at = DateTimeCol(default=None)
+        #due = DateCol(default=None)
+        #tickler = DateCol(default=None)
+        #completed = BoolCol(default=False)
+        #next_todo = IntCol(default=None)
+        #previous_todo = IntCol(default=None)
+
+    # TODO: how the fuck can I for the use of innodb for mysql ?
+    #def connect(selfuser = None, password = None, db_type = None):
+    def connect(self):
+        """
+        Connect to the database
+        """
+        # todo, generalisation
+        sqlhub.processConnection = connectionForURI('mysql://tasks:toto@localhost/tasks')
+
+    def create_db(self):
+        """
+        Create the database if it isn't already create
+        """
+        #Context.createTable(ifNotExists=True)
+        #Project.createTable(ifNotExists=True)
+        #Item.createTable(ifNotExists=True)
+        self._Todo.createTable(ifNotExists=True)
+
+    def drop_db(self):
+        """
+        Drop the database if it isn't already drop
+        """
+        #Context.dropTable(ifExists=True)
+        #Project.dropTable(ifExists=True)
+        #Item.dropTable(ifExists=True)
+        self._Todo.dropTable(ifExists=True)
+
+    def todo_len(self):
+        return _select_len(self._Todo.select())
+
+    def add_todo(self, new_description):
+        self._Todo(description=new_description)
