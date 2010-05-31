a2ab58b6bd1a5fc0dc457baffc0e61c0968e0c7b
Laurent Peuch <psycojoker@gmail.com>
2010-05-31 19:50:24 +0200

    add a reinitialised method to the test

--- test_tododb.py
+++ test_tododb.py
@@ -6,10 +6,17 @@ import unittest
 import tododb
 
 class MaTest(unittest.TestCase):
-    def test_create_a_db(self):
+    def reinitialise(self):
         tododb.connect()
         tododb.drop_db()
         tododb.create_db()
+
+    def test_create_a_db(self):
+        self.reinitialise()
+
+    def test_add_a_todo(self):
+        self.reinitialise()
+
 
 if __name__ == "__main__":
    unittest.main()
