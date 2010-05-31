6fb7ae53fd8f47adcc10f61f11fc6954b93c4e1f
Laurent Peuch <psycojoker@gmail.com>
2010-05-31 21:09:20 +0200

    Add some doc to the tests

--- test_tododb.py
+++ test_tododb.py
@@ -7,6 +7,9 @@ from tododb import TodoDB, TodoAlreadyExist
 class MaTest(unittest.TestCase):
 
     def reinitialise(self):
+        """
+        Reinitialise the db to make test with a clean one
+        """
         tododb = TodoDB()
         tododb.connect()
         tododb.drop_db()
@@ -14,9 +17,16 @@ class MaTest(unittest.TestCase):
         return tododb
 
     def test_create_a_db(self):
+        """
+        create a clean database to test
+        """
         tododb = self.reinitialise()
 
     def test_add_a_todo(self):
+        """
+        You should be able to add a new todo.
+        This should inscrease the number of todos by one
+        """
         tododb = self.reinitialise()
 
         was = tododb.todo_len()
@@ -26,6 +36,9 @@ class MaTest(unittest.TestCase):
         self.assertEqual(was + 1, tododb.todo_len())
 
     def test_cant_add_two_time_the_same_todo(self):
+        """
+        You shouldn't be able to add two time a todo with the same description
+        """
         tododb = self.reinitialise()
 
         was = tododb.todo_len()
