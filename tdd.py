5c23e8ad2b1b7f076d52ed9df1da5921900238a7
Laurent Peuch <psycojoker@gmail.com>
2010-06-09 11:20:23 +0200

    Adding gplv3

--- tdd.py
+++ tdd.py
@@ -1,6 +1,26 @@
 #!/usr/bin/python
 # -*- coding:Utf-8 -*-
 
+"""
+This file is part of Toudoudone.
+
+Toudoudone is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Toudoudone is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Toudoudone.  If not, see <http://www.gnu.org/licenses/>.
+
+Toudoudone  Copyright (C) 2010  Laurent Peuch <cortex@worlddomination.be>
+"""
+
+
 import exceptions
 
 from sqlobject import *
