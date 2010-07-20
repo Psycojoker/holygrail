20712d5f022d9800f589b540517a4162e8022e89
Laurent Peuch <cortex@worlddomination.be>
2010-07-20 22:43:58 +0000

    update some comments -- unimportant

--- tdd.py
+++ tdd.py
@@ -17,7 +17,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Toudoudone.  If not, see <http://www.gnu.org/licenses/>.
 
-Toudoudone  Copyright (C) 2010  Laurent Peuch <cortex@worlddomination.be>
+Toudoudone  Copyright (C) 2010  Laurent Peuch  <cortex@worlddomination.be>
 """
 
 import sqlobject
