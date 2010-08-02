1f0463ec0354ffc6dbe8c08125428189e92bb354
Laurent Peuch <psycojoker@gmail.com>
2010-08-02 21:55:45 +0200

    meh. assert work in the other way

--- tdd.py
+++ tdd.py
@@ -370,7 +370,7 @@ class TodoDB(object):
         Intern method to check if the database exist and if the database is in a normal state.
         """
         # check that everything if normal (all table created or not created)
-        if (not _Item.tableExists() and not _Todo.tableExists() and not _Project.tableExists() and not _Context.tableExists()) or (_Todo.tableExists() and _Project.tableExists() and _Context.tableExists() and _Item.tableExists()):
+        if not ((not _Item.tableExists() and not _Todo.tableExists() and not _Project.tableExists() and not _Context.tableExists()) or (_Todo.tableExists() and _Project.tableExists() and _Context.tableExists() and _Item.tableExists())):
             print "TodoDB: WARNING: database in a non conform state, will probably bug. Do you need to launch a migration script ?"
         elif not _Todo.tableExists() and not _Project.tableExists() and not _Context.tableExists() and not _Item.tableExists():
             print "TodoDB: DB doesn't exist, I'll create it"
