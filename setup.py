63a9f8563ed804062cdf2f7b08877762f839a714
Laurent Peuch <cortex@worlddomination.be>
2010-10-26 17:49:38 +0200

    update version number in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='HolyGrail',
-      version='0.1 Galahad',
+      version='0.1.1 Galahad',
       description='High level lib to manage the holy quests of your life (in other words GTD next actions)',
       author='Laurent Peuch',
       author_email='cortex@worlddomination.be',
